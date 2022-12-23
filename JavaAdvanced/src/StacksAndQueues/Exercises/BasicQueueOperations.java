7db60a50e65617df379c2677477ce8e5f940ea16
GeorgiIvanov <gogata92@pm.me>
2022-12-23 15:31:03 +0100

    Add new tasks

---
+++ JavaAdvanced/src/StacksAndQueues/Exercises/BasicQueueOperations.java
@@ -0,0 +1,7 @@
+package StacksAndQueues.Exercises;
+
+public class BasicQueueOperations {
+    public static void main(String[] args) {
+        
+    }
+}
--- JavaAdvanced/src/StacksAndQueues/PrinterQueues.java
+++ JavaAdvanced/src/StacksAndQueues/PrinterQueues.java
@@ -2,6 +2,6 @@ package StacksAndQueues;
 
 public class PrinterQueues {
     public static void main(String[] args) {
-        
+
     }
 }
