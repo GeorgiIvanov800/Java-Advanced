58efe00793716c0ddafbbbb3234da179abbf73fe
GeorgiIvanov <gogata92@pm.me>
2022-12-25 11:03:07 +0100

    Add new tasks for Sets and Maps

---
+++ JavaAdvanced/src/SetsAndMaps/SoftUniParty.java
@@ -0,0 +1,41 @@
+package SetsAndMaps;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> VIP = new LinkedHashSet<>();
+        Set<String> regular = new LinkedHashSet<>();
+
+        String reservationNumber = scanner.nextLine();
+
+        while (!reservationNumber.equals("PARTY")) {
+
+            char startingSymbol = reservationNumber.charAt(0);
+
+            boolean isVipRegistration = Character.isDigit(startingSymbol);
+            if (isVipRegistration) {
+                VIP.add(reservationNumber);
+            } else {
+                regular.add(reservationNumber);
+            }
+            reservationNumber = scanner.nextLine();
+        }
+
+        String guestReservation = scanner.nextLine();
+        while (!guestReservation.equals("END")) {
+
+            VIP.remove(guestReservation);
+            regular.remove(guestReservation);
+
+            guestReservation = scanner.nextLine();
+        }
+
+        System.out.println(VIP.size() + regular.size());
+        System.out.println(String.join(System.lineSeparator(), VIP) + String.join(System.lineSeparator(), regular));
+    }
+}
