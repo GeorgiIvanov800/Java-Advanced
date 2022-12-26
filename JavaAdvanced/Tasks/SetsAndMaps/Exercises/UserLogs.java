a3713d50398fb1c850e354232476ceb6e51439cc
GeorgiIvanov <gogata92@pm.me>
2022-12-26 12:46:42 +0100

    New Tasks

---
+++ JavaAdvanced/Tasks/SetsAndMaps/Exercises/UserLogs.java
@@ -0,0 +1,9 @@
+package SetsAndMaps.Exercises;
+
+import java.util.Scanner;
+
+public class UserLogs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
