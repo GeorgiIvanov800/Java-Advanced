0457779e664b0c8957bc5485f907362521586ea4
GeorgiIvanov <gogata92@pm.me>
2022-12-27 13:06:00 +0100

    Read data from a file with a stream

--- JavaAdvanced/Tasks/Streams/demo.java
+++ JavaAdvanced/Tasks/Streams/demo.java
@@ -1,18 +1,28 @@
 package Streams;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Read
 
-        String patch = "D:\\SoftUni Java Course\\JavaAdvancedMay2022\\Streams, Files and Directories" +
+        String path = "D:\\SoftUni Java Course\\JavaAdvancedMay2022\\Streams, Files and Directories" +
                 "\\04.Java-Advanced-Files-and-Streams-Lab-Resources" +
                 "\\input.txt";
 
-        Scanner scanner = new Scanner(System.in);
+        FileInputStream inputStream = new FileInputStream(path);
 
+        Scanner scanner = new Scanner(inputStream);
+        String line = scanner.nextLine();
+
+        while (scanner.hasNextLine()) {
+            System.out.println(line);
+            line = scanner.nextLine();
+
+        }
         //System.out.println("Hello World!");
 
         // Write
