7027a29b88ab78fe8aed9a6e406cc35d0498ca12
GeorgiIvanov <gogata92@pm.me>
2022-12-27 13:02:08 +0100

    Streams

---
+++ JavaAdvanced/Tasks/Streams/demo.java
@@ -0,0 +1,21 @@
+package Streams;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+
+        // Read
+
+        String patch = "D:\\SoftUni Java Course\\JavaAdvancedMay2022\\Streams, Files and Directories" +
+                "\\04.Java-Advanced-Files-and-Streams-Lab-Resources" +
+                "\\input.txt";
+
+        Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("Hello World!");
+
+        // Write
+
+    }
+}
