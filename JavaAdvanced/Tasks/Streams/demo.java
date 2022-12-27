3040bc59efc97e05408706a6725b8aefc4805fcd
GeorgiIvanov <gogata92@pm.me>
2022-12-27 13:28:09 +0100

    Use of Try and Catch

--- JavaAdvanced/Tasks/Streams/demo.java
+++ JavaAdvanced/Tasks/Streams/demo.java
@@ -1,6 +1,7 @@
 package Streams;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -11,9 +12,7 @@ public class demo {
 
         String path = "D:\\SoftUni Java Course\\JavaAdvancedMay2022\\Streams, Files and Directories" +
                 "\\04.Java-Advanced-Files-and-Streams-Lab-Resources" +
-                "\\input.txt";
-
-        FileInputStream inputStream = new FileInputStream(path);
+                "\\input.txt1";
 
         /*Scanner scanner = new Scanner(inputStream);
 
@@ -26,13 +25,18 @@ public class demo {
         }
         *///System.out.println("Hello World!");
 
-        int bytte = inputStream.read();
-
-        while (bytte != -1) {
-            System.out.print((char) bytte);
-            bytte = inputStream.read();
+//        int oneByte = inputStream.read();
+//
+//        while (oneByte != -1) {
+//            System.out.print((char) oneByte);
+//            oneByte = inputStream.read();
+//        }
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            System.out.println("File was found it's ok");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + e.getMessage());
         }
-
 
     }
 }
