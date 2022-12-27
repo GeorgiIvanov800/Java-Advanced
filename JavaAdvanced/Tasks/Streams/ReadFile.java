489c1c3b7ae900289648aaf9d8fb7bb70327333e
GeorgiIvanov <gogata92@pm.me>
2022-12-27 14:27:06 +0100

    New Task WriteToFile

--- JavaAdvanced/Tasks/Streams/ReadFile.java
+++ JavaAdvanced/Tasks/Streams/ReadFile.java
@@ -3,11 +3,12 @@ package Streams;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 public class ReadFile {
     public static void main(String[] args) {
         String path = "input.txt";
-
+        String properties = System.getProperty("user.dir");
         try {
             FileInputStream inputStream = new FileInputStream(path);
             FileOutputStream outputStream = new FileOutputStream("output.txt"); // make a new  txt file
---
+++ JavaAdvanced/Tasks/Streams/WriteToFile.java
@@ -0,0 +1,36 @@
+package Streams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Set;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        //Read the file named "input.txt" that is provided for this exercise and
+        // write all its content to a file while skipping any punctuation. Skip the following symbols: ",", ".", "!", "?".
+
+        String path = "input.txt";
+
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            FileOutputStream outputStream = new FileOutputStream("output2.txt");
+            //",", ".", "!", "?".
+            //symbol != ',' && symbol != '.' && symbol != '!' && symbol != '?'
+            Set<Character> punctuationCodes = Set.of(',', '!' , '?', '.');
+            int read = inputStream.read();
+            while (read != -1) {
+
+                char symbol = (char) read;
+
+                if (!punctuationCodes.contains((char)read)) {
+                    outputStream.write(read);
+
+                }
+                read = inputStream.read();
+            }
+        } catch (IOException ignored) {
+
+        }
+    }
+}
