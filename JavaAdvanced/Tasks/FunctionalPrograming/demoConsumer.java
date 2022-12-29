5ee9d31b8ff697f8b083a208dc667f5714fdb52f
GeorgiIvanov <gogata92@pm.me>
2022-12-29 14:54:07 +0100

    Supplier Consumer Predicate

--- JavaAdvanced/Tasks/FunctionalPrograming/demoConsumer.java
+++ JavaAdvanced/Tasks/FunctionalPrograming/demoConsumer.java
@@ -7,7 +7,8 @@ public class demoConsumer {
         Consumer<String> print = message -> System.out.println(message);
         print.accept("Hello");
     }
-    void print(String message) {
+    void print(String message) {            //instead of using void method we can use Consumer
         System.out.println(message);
+
     }
 }
