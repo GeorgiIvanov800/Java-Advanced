255cf41f2274c684274b061f8bb131f2af8f5f50
GeorgiIvanov <gogata92@pm.me>
2022-12-29 12:41:27 +0100

    add

--- JavaAdvanced/Tasks/FunctionalPrograming/demo.java
+++ JavaAdvanced/Tasks/FunctionalPrograming/demo.java
@@ -1,5 +1,8 @@
 package FunctionalPrograming;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class demo {
     public static void main(String[] args) {
         //Method:
@@ -9,16 +12,19 @@ public class demo {
         // signature -> getPowerOf(int, int )
         // body -> { return.Math.pow(x, pow) }
 
-        // Read numbers arr from console on one line
+        // Read numbers arr
         // Read number int pow
         // Rise and print all numbers from  arr[] to pow
 
         int[] arr = {13, 42, 69, 73};
         int pow = 2;
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(getPowerOf(arr[i], pow));
-        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(getPowerOf(arr[i], pow));
+//        }
+        // make the same thing without using for loop
+        Arrays.stream(arr)
+                .forEach(n -> System.out.println(getPowerOf(n, pow)));
     }
 
     public static double getPowerOf(int x, int pow) {
