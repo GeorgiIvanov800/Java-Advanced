6d812aac7f10adc1fdb10339f2476dacfd1f61cb
Georgi Ivanov <gogata92@pm.me>
2023-01-23 13:10:47 +0100

    Update MatrixOfPalindromes.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/MultidimensionalArrays/Exercises/MatrixOfPalindromes.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/MultidimensionalArrays/Exercises/MatrixOfPalindromes.java
@@ -1,10 +1,32 @@
 package ModuleAdvanced.MultidimensionalArrays.Exercises;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MatrixOfPalindromes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rows = input[0];
+        int cols = input[1];
+
+        String[][] matrix = new String[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                //outside symbol
+                //inside symbol
+                char outside = (char) ('a' + row);
+                char middle = (char) (outside + col);
+                matrix[row][col] = "" + outside + middle + outside;
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
