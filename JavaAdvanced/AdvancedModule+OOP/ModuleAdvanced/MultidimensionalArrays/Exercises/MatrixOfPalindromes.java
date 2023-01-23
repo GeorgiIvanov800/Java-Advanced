b8e3cb60a0f610e1175a38c28d5d505e4bb35707
Georgi Ivanov <gogata92@pm.me>
2023-01-23 13:03:16 +0100

    Create MatrixOfPalindromes.java

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/MultidimensionalArrays/Exercises/MatrixOfPalindromes.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.MultidimensionalArrays.Exercises;
+
+import java.util.Scanner;
+
+public class MatrixOfPalindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
