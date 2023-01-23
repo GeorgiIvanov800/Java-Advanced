16dcf5078f8faaeb0e3fd9694ba6a93da6d30006
Georgi Ivanov <gogata92@pm.me>
2023-01-23 09:27:49 +0100

    Update _01_ReadFile.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Streams/_01_ReadFile.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Streams/_01_ReadFile.java
@@ -7,13 +7,16 @@ import java.io.InputStream;
 import java.nio.file.Files;
 
 public class _01_ReadFile {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String filePath = "C:\\Users\\bott-800\\Documents\\GitHub\\JavaSoftUniCourse\\JavaAdvanced\\AdvancedModule+OOP\\ModuleAdvanced\\Streams\\Files\\input.txt";
 
-        InputStream inputStream = new FileInputStream(filePath);
-        int firstByte = inputStream.read();
-        System.out.println(firstByte);
-        System.out.println((char)firstByte);
-        inputStream.close();
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+            int firstByte = inputStream.read();
+            System.out.println(firstByte);
+            System.out.println((char) firstByte);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
