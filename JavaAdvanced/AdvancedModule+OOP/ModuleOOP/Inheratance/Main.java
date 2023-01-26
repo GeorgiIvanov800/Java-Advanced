fe1d5cf1ffe9da4e4fa36c4c9fd169102620654c
Georgi Ivanov <gogata92@pm.me>
2023-01-26 10:55:56 +0100

    Upgrading task Working with Inheritance

--- JavaAdvanced/AdvancedModule+OOP/ModuleOOP/Inheratance/Main.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleOOP/Inheratance/Main.java
@@ -3,8 +3,9 @@ package ModuleOOP.Inheratance;
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
-
-        dog.eat();
-        dog.bark();
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
     }
 }
