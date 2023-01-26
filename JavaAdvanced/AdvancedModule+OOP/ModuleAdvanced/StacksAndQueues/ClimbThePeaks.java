ed19f9117022d5f4957541357193204f1742c01f
Georgi Ivanov <gogata92@pm.me>
2023-01-26 09:21:06 +0100

    Create ClimbThePeaks.java

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/StacksAndQueues/ClimbThePeaks.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.StacksAndQueues;
+
+import java.util.Scanner;
+
+public class ClimbThePeaks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
