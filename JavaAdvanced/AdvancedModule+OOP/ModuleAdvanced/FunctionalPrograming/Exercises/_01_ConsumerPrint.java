56f3e42dd22522b7a840215e6ed07d50e2b3cdfe
Georgi Ivanov <gogata92@pm.me>
2023-01-27 08:50:51 +0100

    Add more Exercises in Package Functional Programming
    Exercising working with Consumer Function Supplier Predicate etc.

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/FunctionalPrograming/Exercises/_01_ConsumerPrint.java
@@ -0,0 +1,16 @@
+package ModuleAdvanced.FunctionalPrograming.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+//Write a program that reads a collection of strings, separated by one or more whitespaces,
+// from the console and then prints them onto the console. Each string should be printed on a new line.
+public class _01_ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        Consumer<String> printNames = name -> System.out.println(name);
+        Arrays.stream(input).forEach(printNames);
+    }
+}
