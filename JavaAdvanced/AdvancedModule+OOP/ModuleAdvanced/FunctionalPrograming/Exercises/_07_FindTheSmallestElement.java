080f28e44607887ce2dfb94c036eb30feb317484
Georgi Ivanov <gogata92@pm.me>
2023-01-27 10:46:25 +0100

    Update find smallest element

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/FunctionalPrograming/Exercises/_07_FindTheSmallestElement.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/FunctionalPrograming/Exercises/_07_FindTheSmallestElement.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 //Write a program that is using a custom function  to find the smallest integer in a sequence of integers and print the index of that element.
 
@@ -13,9 +14,14 @@ public class _07_FindTheSmallestElement {
         Scanner scanner = new Scanner(System.in);
         List<Integer> numbersList = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(ele -> Integer.parseInt(ele)).collect(Collectors.toList());
-        int minElement = Collections.min(numbersList);
+
+        //int minElement = Collections.min(numbersList);
         //list -> print the most right (last index of the smallest number in the list)
-        Consumer<List<Integer>> printLastIndexOfMinElement = list -> System.out.println(list.lastIndexOf(minElement));
-        printLastIndexOfMinElement.accept(numbersList);
+
+//        Consumer<List<Integer>> printLastIndexOfMinElement = list -> System.out.println(list.lastIndexOf(minElement));
+//        printLastIndexOfMinElement.accept(numbersList);
+
+        Function<List<Integer>, Integer> getLastIndexOfMinElement = list -> list.lastIndexOf(Collections.min(list));
+        System.out.println(getLastIndexOfMinElement.apply(numbersList));
     }
 }
---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/FunctionalPrograming/Exercises/_08_CustomComparator.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.FunctionalPrograming.Exercises;
+
+import java.util.Scanner;
+
+public class _08_CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
