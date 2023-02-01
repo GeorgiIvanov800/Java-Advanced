97e236ddd88d7e343ab1170b1066a7547eb536e6
Georgi Ivanov <gogata92@pm.me>
2023-02-01 09:26:48 +0100

    Custom Array Testing in Main
    done some testing of the implement method resize and add element in the custom array

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/Main.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/Main.java
@@ -1,5 +1,15 @@
 package ModuleAdvanced.WorkShopCustomDataStructure;
 
 public class Main {
+    public static void main(String[] args) {
+        CustomArray customArray = new CustomArray();
 
+        customArray.add(5);
+        customArray.add(5);
+        customArray.add(5);
+        customArray.add(5);
+        customArray.add(100);
+
+        System.out.println();
+    }
 }
