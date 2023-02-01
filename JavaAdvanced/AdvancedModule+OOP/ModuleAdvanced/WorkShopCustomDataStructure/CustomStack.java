1f675f3f3b6e881d2bcfe2948a80ff72989c8ea1
Georgi Ivanov <gogata92@pm.me>
2023-02-01 11:18:42 +0100

    Add method 'pop' in Custom stack
    Removes the last element in the Stack

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/CustomStack.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/CustomStack.java
@@ -1,5 +1,7 @@
 package ModuleAdvanced.WorkShopCustomDataStructure;
 
+import java.util.NoSuchElementException;
+
 public class CustomStack {
     private static final int INITIAL_CAPACITY = 4;
     private int[] data;
@@ -19,7 +21,20 @@ public void push(int element) {
     this.data[this.size] = element;
     this.size++;
 }
+public int pop() {
+        if (this.size == 0) {
+            throw new NoSuchElementException("Not going to happen there is no elements in the Stack");
+        }
+        int element = this.data[this.size - 1];
+        this.data[this.size - 1] = 0;
+        this.size--;
 
+        if (this.size <= this.capacity / 4) {
+            shrink();
+        }
+
+        return element;
+}
 
 
     private void resize() {
@@ -27,6 +42,17 @@ public void push(int element) {
         int[] copyArr = new int[this.capacity];
 
         System.arraycopy(this.data, 0, copyArr, 0, this.data.length);
+        this.data = copyArr;
+    }
+
+
+    private void shrink() {
+        this.capacity /= 2;
+        int[] copyArr = new int[this.capacity];
+
+        for (int i = 0; i < this.size; i++) {
+            copyArr[i] = this.data[i];
+        }
         this.data = copyArr;
     }
 }
