8d596ce995b25c70c16d649b79ff7b322076b8b2
Georgi Ivanov <gogata92@pm.me>
2023-02-01 09:13:35 +0100

    Defining the Custom Array Class
    Created the field

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/CustomArray.java
@@ -0,0 +1,16 @@
+package ModuleAdvanced.WorkShopCustomDataStructure;
+
+public class CustomArray {
+    //•	void add(int element) - Adds the given element to the end of the list
+    //•	int get(int index) - Returns the element at the specified position in this list
+    //•	int remove(int index) - Removes the element at the given index
+    //•	bool contains(int element) - Checks if the list contains the given element returns (True or False)
+    //•	void add(int firstIndex, int secondIndex) - Adds element at the specific index, the element at this index gets shifted to the right alongside any following elements, increasing the size
+    //•	void forEach(Consumer<Integer> consumer) - Goes through each one of the elements in the list
+
+    private static final int INITIAL_CAPACITY = 10;
+    private int[] data;
+    private int size;
+    private int capacity;
+    
+}
