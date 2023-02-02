913a90de08073c4bef25bec1399c2818e23c03b9
GeorgiIvanov <gogata92@pm.me>
2023-02-02 19:32:01 +0100

    Implemented method toArray in DoublyLinkedList -> returns the collection as an array.

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/DoublyLinkedList.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/DoublyLinkedList.java
@@ -6,10 +6,8 @@ import java.util.function.Consumer;
 public class DoublyLinkedList {
 
 
-
-//void forEach() – goes through the collection and executes a given action
-//int[] toArray() – returns the collection as an array
-static class ListNode {
+    //int[] toArray() – returns the collection as an array
+    static class ListNode {
         int element;
         ListNode next;
         ListNode prev;
@@ -24,7 +22,7 @@ static class ListNode {
     private int size;
 
 
-// void addFirst(int element) – adds an element at the beginning of the collection
+    // void addFirst(int element) – adds an element at the beginning of the collection
     public void addFirst(int element) {
         ListNode newNode = new ListNode(element);
 
@@ -38,7 +36,7 @@ static class ListNode {
         this.size++;
     }
 
-//void addLast(int element) – adds an element at the end of the collection
+    //void addLast(int element) – adds an element at the end of the collection
     public void addLast(int element) {
         ListNode newNode = new ListNode(element);
 
@@ -53,12 +51,12 @@ static class ListNode {
     }
 
 
-//int get(int index) - Returns the element at the specified position in this list
+    //int get(int index) - Returns the element at the specified position in this list
     public int get(int index) {
         validateIndex(index);
 
 
-        if (index <= this.size / 2 ) {
+        if (index <= this.size / 2) {
             ListNode currentNode = this.head;
             for (int i = 0; i < index; i++) {
                 currentNode = currentNode.next;
@@ -66,7 +64,7 @@ static class ListNode {
             return currentNode.element;
         } else {
             ListNode currentNode = this.tail;
-            for (int i = this.size - 1; i > index ; i--) {
+            for (int i = this.size - 1; i > index; i--) {
                 currentNode = currentNode.prev;
             }
             return currentNode.element;
@@ -74,7 +72,7 @@ static class ListNode {
     }
 
 
-//int removeFirst() – removes the element at the beginning of the collection
+    //int removeFirst() – removes the element at the beginning of the collection
     public int removeFirst() {
         checkEmpty();
         int fistElement = this.head.element;
@@ -90,23 +88,23 @@ static class ListNode {
         return fistElement;
     }
 
-//int removeLast() – removes the element at the end of the collection
+    //int removeLast() – removes the element at the end of the collection
     public int removeLast() {
 
-    int element = this.tail.element;
-    this.tail = this.tail.prev;
+        int element = this.tail.element;
+        this.tail = this.tail.prev;
 
-    if (this.tail == null) {
-        this.head = null;
-    } else {
-        this.tail.next = null;
+        if (this.tail == null) {
+            this.head = null;
+        } else {
+            this.tail.next = null;
+        }
+
+        this.size--;
+        return element;
     }
 
-    this.size--;
-    return element;
-    }
-
-//void forEach() – goes through the collection and executes a given action
+    //void forEach() – goes through the collection and executes a given action
     public void forEach(Consumer<Integer> consumer) {
         ListNode currentNode = this.head;
         while (currentNode != null) {
@@ -115,11 +113,26 @@ static class ListNode {
         }
     }
 
+    //int[] toArray() – returns the collection as an array
+    public int[] toArray() {
+        int[] array = new int[this.size];
+        ListNode currentNode = this.head;
+        int counter = 0;
+        while (currentNode != null) {
+            array[counter] = currentNode.element;
+            counter++;
+            currentNode = currentNode.next;
+        }
+
+        return array;
+    }
+
     private void validateIndex(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException("What the hell are you thinking index is out of BOUNDS!");
         }
     }
+
     private void checkEmpty() {
         if (this.size == 0) {
             throw new NoSuchElementException("Node is empty");
