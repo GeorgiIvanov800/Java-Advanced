702e026bb2ba8dd0769919101dccd1d404e80fb2
GeorgiIvanov <gogata92@pm.me>
2023-02-02 18:04:08 +0100

    Add new Custom Data Structure DoublyLinkedList

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/WorkShopCustomDataStructure/DoublyLinkedList.java
@@ -0,0 +1,31 @@
+package ModuleAdvanced.WorkShopCustomDataStructure;
+
+public class DoublyLinkedList {
+    // void addFirst(int element) – adds an element at the beginning of the collection
+//void addLast(int element) – adds an element at the end of the collection
+//int get(int index) - Returns the element at the specified position in this list
+//int removeFirst() – removes the element at the beginning of the collection
+//int removeLast() – removes the element at the end of the collection
+//void forEach() – goes through the collection and executes a given action
+//int[] toArray() – returns the collection as an array
+    class ListNode {
+        int element;
+        ListNode next;
+        ListNode prev;
+
+        public ListNode(int element) {
+            this.element = element;
+        }
+    }
+
+    private ListNode head;
+    private ListNode tail;
+    private int size;
+
+
+    // void addFirst(int element) – adds an element at the beginning of the collection
+    public void addFirst(int element) {
+
+    }
+}
+
