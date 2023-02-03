6c34b42ad9a737c86cc48911479664d8fc06ac04
Georgi Ivanov <gogata92@pm.me>
2023-02-03 10:30:00 +0100

    Update Ctizen.java
    Created an Interface Person and a Class Citizen that implements the method from the Interface

--- JavaAdvanced/AdvancedModule+OOP/ModuleOOP/AbstractClassAndInterface/Ctizen.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleOOP/AbstractClassAndInterface/Ctizen.java
@@ -1,4 +1,22 @@
 package ModuleOOP.AbstractClassAndInterface;
 
-public class Ctizen {
+public class Ctizen implements  Person {
+
+    private String name;
+    private int age;
+
+    public Ctizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
 }
