8063cfcc4f038b96209827699937f2b7ef02d636
GeorgiIvanov <gogata92@pm.me>
2023-02-04 18:26:52 +0100

    Add two methods in class Container addItem and removeItem

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Generics/Container.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Generics/Container.java
@@ -1,5 +1,20 @@
 package ModuleAdvanced.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Container <T>{
-    
+    private List<T> items;
+
+    public Container() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(T item) {
+        this.items.add(item);
+    }
+
+    public boolean removeItem(T item) {
+        return this.items.remove(item);
+    }
 }
