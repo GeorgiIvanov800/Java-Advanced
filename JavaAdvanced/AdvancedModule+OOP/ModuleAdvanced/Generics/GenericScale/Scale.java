c29fbe059ecf93d162990fcef0be0cf2d900d59f
GeorgiIvanov <gogata92@pm.me>
2023-02-04 20:30:31 +0100

    Add method getHeavier into class Scale

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Generics/GenericScale/Scale.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/Generics/GenericScale/Scale.java
@@ -1,4 +1,22 @@
 package ModuleAdvanced.Generics.GenericScale;
 
-public class Scale {
+public class Scale<T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        if (left.compareTo(right) < 0) {
+            return right;
+        }
+        if (left.compareTo(right) == 0) {
+            return left;
+        } else {
+            return null;
+        }
+    }
 }
