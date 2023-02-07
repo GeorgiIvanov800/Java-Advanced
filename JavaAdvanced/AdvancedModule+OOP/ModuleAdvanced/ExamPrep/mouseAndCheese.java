6e3337c31ab59f4fe52284e9d4f0cec2e8ac50ab
Georgi Ivanov <gogata92@pm.me>
2023-02-07 10:48:03 +0100

    Create mouseAndCheese.java
    Exam task working and understanding the multidimensional arrays

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/mouseAndCheese.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.ExamPrep;
+
+import java.util.Scanner;
+
+public class mouseAndCheese {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
