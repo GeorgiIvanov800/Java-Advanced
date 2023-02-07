56a8b97cdada38ae267eb15408bbe35cc593689b
Georgi Ivanov <gogata92@pm.me>
2023-02-07 09:12:28 +0100

    Update Cooking.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Cooking.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Cooking.java
@@ -1,4 +1,10 @@
 package ModuleAdvanced.ExamPrep;
 
+import java.util.Scanner;
+
 public class Cooking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
