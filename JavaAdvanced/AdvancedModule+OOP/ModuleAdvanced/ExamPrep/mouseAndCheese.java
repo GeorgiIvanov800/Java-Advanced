ef2bd6db5ea0d7cf344a67fb84f7fedb46f73223
Georgi Ivanov <gogata92@pm.me>
2023-02-07 11:00:39 +0100

    Update mouseAndCheese.java
    fill the 2d array

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/mouseAndCheese.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/mouseAndCheese.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class mouseAndCheese {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        char[][] field = new char[size][size];
+
+        for (int i = 0; i < size; i++) {
+            field[i] = scanner.nextLine().toCharArray();
+        }
+
+
+        print2dArray(field);
+    }
+
+    public static void print2dArray(char[][] field) {
+        for (char[] arr : field) {
+            for (char c : arr) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
     }
 }
