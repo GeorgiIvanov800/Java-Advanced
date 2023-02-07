d78d39d0f75373a33d062765df5599c04fadd297
Georgi Ivanov <gogata92@pm.me>
2023-02-07 09:28:05 +0100

    Update Cooking.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Cooking.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Cooking.java
@@ -1,10 +1,31 @@
 package ModuleAdvanced.ExamPrep;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Cooking {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Read input first line is queue second is stack
+
+        ArrayDeque<Integer> liquids = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> ingredients = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(ele -> ingredients.push(ele));
+        System.out.println();
+        //sum first from queue with second from stack
         
+
+        //check the sum according to the table and do the correct thing
+
+        //Check and print output
+
+
     }
 }
