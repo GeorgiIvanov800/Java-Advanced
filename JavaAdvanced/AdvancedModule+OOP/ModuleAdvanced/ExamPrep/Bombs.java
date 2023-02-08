2fa0d68e3383c03dd438b6f60a4da2d355d92a62
Georgi Ivanov <gogata92@pm.me>
2023-02-08 13:18:21 +0100

    Update Bombs.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Bombs.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Bombs.java
@@ -7,15 +7,15 @@ import java.util.stream.Collectors;
 
 public class Bombs {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       int matrixSize = Integer.parseInt(scanner.nextLine());
-       String[] commands = scanner.nextLine().split(",");
+        Scanner scanner = new Scanner(System.in);
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+        String[] commands = scanner.nextLine().split(",");
 
-       char[][]matrix = new char[matrixSize][matrixSize];
+        char[][] matrix = new char[matrixSize][matrixSize];
 
-       int sapperRow = 0;
-       int sapperCol = 0;
-       int bombCounter = 0;
+        int sapperRow = 0;
+        int sapperCol = 0;
+        int bombCounter = 0;
 
         for (int row = 0; row < matrixSize; row++) {
             List<Character> characterList = Arrays.stream(scanner.nextLine().split(" "))
@@ -33,6 +33,30 @@ public class Bombs {
             }
         }
 
-        
+        for (int i = 0; i < commands.length; i++) {
+            String commandName = commands[i];
+            switch (commandName) {
+                case "up":
+                    if (sapperRow != 0) {
+                        sapperRow--;
+                    }
+                    break;
+                case "down":
+                    if (sapperRow != matrixSize - 1) {
+                        sapperRow++;
+                    }
+                    break;
+                case "right":
+                    if (sapperCol != matrixSize - 1) {
+                        sapperCol++;
+                    }
+                    break;
+                case "left":
+                    if (sapperCol != 0) {
+                        sapperCol--;
+                    }
+                    break;
+            }
+        }
     }
 }
