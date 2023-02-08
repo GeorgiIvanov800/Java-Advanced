f49dd4c2e8d55a77adc04db58c7d6c38ccdc8530
Georgi Ivanov <gogata92@pm.me>
2023-02-08 09:33:11 +0100

    Update Flowers.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Flowers.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Flowers.java
@@ -1,10 +1,35 @@
 package ModuleAdvanced.ExamPrep;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Flowers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        ArrayDeque<Integer> liliesStack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .forEach(ele -> liliesStack.push(ele));
+
+        ArrayDeque<Integer> rosesQueue = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        int wreathCount = 0;
+
+        if (!liliesStack.isEmpty() && !rosesQueue.isEmpty()) {
+            int lily = liliesStack.pop();
+            int rose = rosesQueue.poll();
+
+            int sum = lily + rose;
+
+            if (sum == 15) {
+                wreathCount++;
+            }
+        }
     }
 }
