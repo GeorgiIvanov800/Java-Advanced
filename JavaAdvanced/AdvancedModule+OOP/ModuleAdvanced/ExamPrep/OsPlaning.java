a28fb8f4e90c988484148ff30910eda4140c3bbe
Georgi Ivanov <gogata92@pm.me>
2023-02-09 09:46:35 +0100

    Update OsPlaning and add new exam task ReVolt

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/OsPlaning.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/OsPlaning.java
@@ -1,7 +1,43 @@
 package ModuleAdvanced.ExamPrep;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class OsPlaning {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<Integer> tasksStack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .forEach(ele -> tasksStack.push(ele));
+
+        ArrayDeque<Integer> threadQueue = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        int taskValue = Integer.parseInt(scanner.nextLine());
+
+        int task = tasksStack.peek();
+        int thread = threadQueue.peek();
+
+        while (task != taskValue) {
+
+            if (thread >= task) {
+                tasksStack.pop();
+            }
+
+            threadQueue.poll();
+
+
+            task = tasksStack.peek();
+            thread = threadQueue.peek();
+        }
+        System.out.printf("Thread with value %d killed task %d%n", threadQueue.peek(), taskValue);
+        String leftThreads = threadQueue.stream().map(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(leftThreads);
     }
 }
---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/ReVolt.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.ExamPrep;
+
+import java.util.Scanner;
+
+public class ReVolt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
