4b4a947241b3d259c5551b0d498ff54809dc14fd
Georgi Ivanov <gogata92@pm.me>
2023-02-11 14:13:57 +0100

    Create PastryShop.java

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/PastryShop.java
@@ -0,0 +1,9 @@
+package ModuleAdvanced.ExamPrep;
+
+import java.util.Scanner;
+
+public class PastryShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
