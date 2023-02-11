1e1c586e940a9be7245346231a531ae489a36761
Georgi Ivanov <gogata92@pm.me>
2023-02-11 14:55:42 +0100

    Created new Task in Exam Prep 'Snake'
    New Task Snake for the exam
    How to work with 2D Arrays

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/PastryShop.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/PastryShop.java
@@ -63,6 +63,6 @@ public class PastryShop {
             System.out.println("Ingredients left: " + remainingIngridients);
         }
 
-        
+
     }
 }
---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Snake.java
@@ -0,0 +1,9 @@
+package ModuleAdvanced.ExamPrep;
+
+import java.util.Scanner;
+
+public class Snake {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
