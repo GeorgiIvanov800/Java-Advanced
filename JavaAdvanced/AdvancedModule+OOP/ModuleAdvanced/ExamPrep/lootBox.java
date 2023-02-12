d212b197bbeecc4c5b97ed81f78859535399044b
Georgi Ivanov <gogata92@pm.me>
2023-02-12 11:23:16 +0100

    Add new task in ExamPrep LootBox

---
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/lootBox.java
@@ -0,0 +1,10 @@
+package ModuleAdvanced.ExamPrep;
+
+import java.util.Scanner;
+
+public class lootBox {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
