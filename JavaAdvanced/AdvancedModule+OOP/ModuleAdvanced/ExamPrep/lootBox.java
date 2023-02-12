671486df4005026ca242efa75d3aff473a583f1f
Georgi Ivanov <gogata92@pm.me>
2023-02-12 11:39:20 +0100

    Update lootBox.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/lootBox.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/lootBox.java
@@ -1,10 +1,22 @@
 package ModuleAdvanced.ExamPrep;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class lootBox {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        List<Integer> loot = new ArrayList<>();
+
+        ArrayDeque<Integer> firstBoxQueue = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        ArrayDeque<Integer> secondBoxStack = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .forEach(ele -> secondBoxStack.push(ele));
+
+        System.out.println();
     }
 }
