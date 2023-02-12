4ea57f7556a5dc100dab6ce582baa5fbd0f5cd81
Georgi Ivanov <gogata92@pm.me>
2023-02-12 12:16:25 +0100

    Update Bee.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Bee.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/Bee.java
@@ -5,6 +5,33 @@ import java.util.Scanner;
 public class Bee {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int size = Integer.parseInt(scanner.nextLine());
+
+        char[][] territory = new char[size][size];
+
+        for (int row = 0; row < territory.length; row++) {
+            String input = scanner.nextLine();
+            territory[row] = input.toCharArray();
+
+        }
+
+        printMatrix(territory);
+    }
+
+
+
+
+
+
+
+
+
+    private static void printMatrix(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
