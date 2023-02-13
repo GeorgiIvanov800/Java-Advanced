d5d90634532b6b0561a6c9f4b89e8c06477ac9ea
Georgi Ivanov <gogata92@pm.me>
2023-02-13 13:54:25 +0100

    Update test2.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/DefineClass/test2.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/DefineClass/test2.java
@@ -4,68 +4,50 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class test2 {
-    private static List<String> text2 = new ArrayList<>();
+    private static List<String> outputText = new ArrayList<>();
+    private static Map<String, Integer> pattern = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        List<Character> text = input.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
-        Map<String, Integer> pattern = new LinkedHashMap<>();
+        List<Character> inputText = input.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
 
-        for (char currentSymbol : text) {
+
+        for (char currentSymbol : inputText) {
+
             String symbol = String.valueOf(currentSymbol);
-           if (!pattern.containsKey(symbol)) {
-               pattern.put(symbol, 1);
-           } else {
-               pattern.put(symbol, pattern.get(symbol) + 1);
-           }
+
+            if (!pattern.containsKey(symbol)) {
+                pattern.put(symbol, 1);
+            } else {
+                pattern.put(symbol, pattern.get(symbol) + 1);
+            }
         }
 
-        fillList(pattern);
 
-        text2.stream().forEach(e -> System.out.print(e));
+        fillList(outputText, pattern);
+
+        outputText.stream().forEach(e -> System.out.print(e));
         System.out.println();
 
     }
 
 
-
-
-
-    private static void fillList(Map<String, Integer> pattern) {
+    private static void fillList(List<String> outputText, Map<String, Integer> pattern) {
         for (var entry : pattern.entrySet()) {
             String symbol = entry.getKey();
             int count = entry.getValue();
 
             if (count >= 4) {
-                text2.add("$");
-                text2.add(String.valueOf(count));
-                text2.add(symbol);
+                outputText.add("$");
+                outputText.add(String.valueOf(count));
+                outputText.add(symbol);
             } else {
                 for (int j = 0; j < count; j++) {
-                    text2.add(symbol);
+                    outputText.add(symbol);
                 }
             }
         }
     }
 
-
-    private static void countSymbol(List text, int count, String symbol) {
-
-        for (int i = 1; i <= count; i++) {
-            if (count >= 4) {
-                text.add("$");
-                String a = String.valueOf(count);
-                text.add(a);
-                text.add(symbol);
-                break;
-            } else {
-                for (int j = 0; j < count; j++) {
-                    text.add(symbol);
-
-                }
-                break;
-            }
-        }
-    }
 }
