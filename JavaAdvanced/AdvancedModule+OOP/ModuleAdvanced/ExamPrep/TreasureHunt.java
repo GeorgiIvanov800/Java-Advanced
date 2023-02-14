ebb32ff8204d5d32024732bfb44d25202531b7ee
Georgi Ivanov <gogata92@pm.me>
2023-02-14 21:17:30 +0100

    Update TreasureHunt.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/TreasureHunt.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/TreasureHunt.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TreasureHunt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = Integer.parseInt(scanner.nextLine());
+        int m = Integer.parseInt(scanner.nextLine());
     }
 }
