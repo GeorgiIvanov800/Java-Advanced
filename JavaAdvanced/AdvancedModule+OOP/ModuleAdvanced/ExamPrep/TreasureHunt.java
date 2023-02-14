f87103aa9958cc221b30a462b883b6842a23fe92
Georgi Ivanov <gogata92@pm.me>
2023-02-14 21:16:06 +0100

    Update TreasureHunt.java

--- JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/TreasureHunt.java
+++ JavaAdvanced/AdvancedModule+OOP/ModuleAdvanced/ExamPrep/TreasureHunt.java
@@ -1,5 +1,10 @@
 package ModuleAdvanced.ExamPrep;
 
+import java.util.Scanner;
+
 public class TreasureHunt {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
